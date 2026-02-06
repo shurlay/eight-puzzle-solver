b7463fd9fcbe144ebb15f59c6346c474b81f69a4
Shirley Duong <shirleytduong@gmail.com>
2026-02-05 18:21:25 -0800

    implement uniform cost search & fix push/pop

--- cs170proj1.py
+++ cs170proj1.py
@@ -1,12 +1,14 @@
 import heapq as heapq
 
-test_puzzle = [[0, 1, 2],
-               [4, 5, 3],
-               [7, 8, 6]]
+insertion_counter = 0
 
-goal_state = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 0]]
+test_puzzle = (0, 1, 2,
+               4, 5, 3,
+               7, 8, 6)
+
+goal_state = (1, 2, 3,
+              4, 5, 6,
+              7, 8, 0)
 
 class Node:
     def __init__(self, state, path_cost = 0):
@@ -19,21 +21,25 @@ def general_search(puzzle, queueing_function):
     while True:
         if not nodes:
             return "failure"
-        
+
         node = remove_front(nodes)
 
         if goal_test(node.state):
+            for i in range(0, 9, 3):
+                print(node.state[i:i+3])
             return node
         
         nodes = queueing_function(nodes, expand(node))
 
 def make_queue(node):
+    global insertion_counter
     queue = []
-    heapq.heappush(queue, node)
+    heapq.heappush(queue, (node.path_cost, insertion_counter, node))
+    insertion_counter += 1
     return queue
 
 def remove_front(nodes):
-    node = heapq.heappop(nodes)
+    _,_, node = heapq.heappop(nodes)
     return node
 
 def goal_test(state):
@@ -52,13 +58,13 @@ def possible_moves(state):
     n = int(len(state) ** 0.5)
 
     if empty_spot % n != 0:
-        moves.append.move(state, empty_spot, empty_spot - 1)
+        moves.append(move(state, empty_spot, empty_spot - 1))
     if empty_spot % n != n - 1:
-        moves.append.move(state, empty_spot, empty_spot + 1)
+        moves.append(move(state, empty_spot, empty_spot + 1))
     if empty_spot >= n:
-        moves.append.move(state, empty_spot, empty_spot - n)
+        moves.append(move(state, empty_spot, empty_spot - n))
     if empty_spot < n * (n - 1):
-        moves.append.move(state, empty_spot, empty_spot + n)
+        moves.append(move(state, empty_spot, empty_spot + n))
     
     return moves
 
@@ -69,16 +75,26 @@ def move(state, empty_spot, neighbor):
     new_state[neighbor] = temp
     return tuple(new_state)
 
+def uniform_cost_search(nodes, children):
+    global insertion_counter
+    for child in children:
+        heapq.heappush(nodes, (child.path_cost, insertion_counter, child))
+        insertion_counter += 1
+    return nodes
+
 def select_algorithm(puzzle):
     algorithm = input("Choose an algorithm: '1' for Uniform Cost Search, '2' for A* " +
     "with the Misplaced Tile Heuristic, or '3' for the A* with the Manhattan Distance Heuristic.\n")
 
     if algorithm == "1":
         general_search(puzzle, uniform_cost_search)
-    if algorithm == "2":
-        general_search(puzzle, misplaced_title)
-    if algorithm == "3":
-        general_search(puzzle, manhattan_distance)
+    # if algorithm == "2":
+    #     general_search(puzzle, misplaced_title)
+    # if algorithm == "3":
+    #     general_search(puzzle, manhattan_distance)
 
 def main():
     select_algorithm(test_puzzle)
+
+if __name__ == "__main__":
+    main()
